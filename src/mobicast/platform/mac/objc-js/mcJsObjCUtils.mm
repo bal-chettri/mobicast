de38213fbd920ad2b3a5c3f49dc83c4901652ff6
Bal Chettri <balu.chettri@gmail.com>
2018-11-22 01:16:58 +0530

    - Manual implementation of `js2objcArray` & `js2objcDictionary`.
    Avoid use of inbuilt conversions from JSValue like `toArray` as they don't support custom objects.

--- src/mobicast/platform/mac/objc-js/mcJsObjCUtils.mm
+++ src/mobicast/platform/mac/objc-js/mcJsObjCUtils.mm
@@ -3,6 +3,7 @@
  * Licensed under GNU GPL(https://www.gnu.org/licenses/gpl.html)
  */
 #import <mobicast/platform/mac/objc-js/mcJsObjCUtils.h>
+#import <mobicast/platform/mac/mcAppDelegate.h>
 #import <JavaScriptCore/JavaScriptCore.h>
 #include <mobicast/mcDebug.h>
 
@@ -12,14 +13,76 @@
 {
     JSValue *jsVal = [obj JSValue];
     MC_ASSERT([jsVal isArray] == TRUE);
-    return [jsVal toArray];
+
+    NSUInteger count = [[obj valueForKey:@"length"] integerValue];
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:count];
+
+    for (unsigned i = 0; i < count; i++)
+    {
+        id elemValue = [obj webScriptValueAtIndex:i];
+
+        if([elemValue isKindOfClass:[WebScriptObject class]])
+        {
+            JSValue *elemJsVal = [elemValue JSValue];
+
+            if([elemJsVal isArray])
+            {
+                elemValue = [MCJsObjCUtils js2objcArray:elemValue];
+            }
+            else
+            {
+                MC_ASSERT(false);
+            }
+        }
+
+        [array addObject:elemValue];
+    }
+
+    return array;
 }
 
 + (NSDictionary *)js2objcDictionary:(WebScriptObject *)obj
 {
     JSValue *jsVal = [obj JSValue];
     MC_ASSERT([jsVal isObject] == TRUE);
-    return [jsVal toDictionary];
+
+    JSContextRef jsContext = [MCAppDelegate instance].webview.mainFrame.globalContext;
+
+    JSPropertyNameArrayRef props = JSObjectCopyPropertyNames(jsContext, [obj JSObject]);
+    size_t count = JSPropertyNameArrayGetCount(props);
+
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:count];
+
+    for(NSInteger i = 0; i < count; i++)
+    {
+        JSStringRef propName = JSPropertyNameArrayGetNameAtIndex(props, i);
+        NSString *strPropName = (NSString *)CFBridgingRelease(JSStringCopyCFString(kCFAllocatorDefault, propName));
+
+        id elemValue = [obj valueForKey:strPropName];
+
+        if([elemValue isKindOfClass:[WebScriptObject class]])
+        {
+            JSValue *elemJsVal = [elemValue JSValue];
+            if([elemJsVal isArray])
+            {
+                elemValue = [MCJsObjCUtils js2objcArray:elemValue];
+            }
+            else if([elemJsVal isObject])
+            {
+                elemValue = [MCJsObjCUtils js2objcDictionary:elemValue];
+            }
+            else
+            {
+                MC_ASSERT(false);
+            }
+        }
+
+        [dict setObject:elemValue forKey:strPropName];
+    }
+
+    JSPropertyNameArrayRelease(props);
+
+    return dict;
 }
 
 @end
